43ef77016c7f6c909c4a9c9b25efc051474acbd5
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-05-24 17:57:45 +0700

    `update` weather repository

--- app/src/main/java/com/agronify/android/model/repository/WeatherRepository.kt
+++ app/src/main/java/com/agronify/android/model/repository/WeatherRepository.kt
@@ -1,4 +1,41 @@
 package com.agronify.android.model.repository
 
-class WeatherRepository {
+import com.agronify.android.model.remote.response.CurrentWeatherResponse
+import com.agronify.android.model.remote.response.ForecastWeatherResponse
+import com.agronify.android.model.remote.retrofit.ApiService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import retrofit2.await
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Singleton
+class WeatherRepository @Inject constructor(
+    private val apiService: ApiService,
+    @Named("networkDispatcher") private val networkDispatcher: CoroutineDispatcher
+){
+    suspend fun getCurrentWeather(
+        lat: Float,
+        lon: Float
+    ): CurrentWeatherResponse {
+        return withContext(networkDispatcher) {
+            apiService.getCurrentWeather(CURRENT, lat, lon, TIMEZONE).await()
+        }
+    }
+
+    suspend fun getForecastWeather(
+        lat: Float,
+        lon: Float
+    ): ForecastWeatherResponse {
+        return withContext(networkDispatcher) {
+            apiService.getForecastWeather(FORECAST, lat, lon, TIMEZONE).await()
+        }
+    }
+
+    companion object {
+        private const val CURRENT = "current"
+        private const val FORECAST = "forecast"
+        private const val TIMEZONE = "Asia/Jakarta"
+    }
 }
