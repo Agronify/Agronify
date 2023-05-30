5bfa54e9de8511cdf38345e3ef68f48ae4a8bba4
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-05-30 13:21:22 +0700

    `update` onboard with auth

--- app/src/main/java/com/agronify/android/view/activity/main/OnboardActivity.kt
+++ app/src/main/java/com/agronify/android/view/activity/main/OnboardActivity.kt
@@ -1,12 +1,122 @@
 package com.agronify.android.view.activity.main
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.agronify.android.R
+import com.agronify.android.databinding.ActivityOnboardBinding
+import com.agronify.android.viewmodel.OnboardViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OnboardActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityOnboardBinding.inflate(layoutInflater)
+    }
+    private val onboardViewModel: OnboardViewModel by viewModels()
+    private var newUser: Boolean = false
+    private var hasLoaded: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboard)
+        setContentView(binding.root)
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        hideStatusBar()
+        checkUser()
+        installSplash()
+    }
+
+    private fun setupAction() {
+        binding.apply {
+            btnSkip.setOnClickListener {
+                navigateToMain()
+            }
+
+            btnLogin.setOnClickListener {
+                navigateToLogin()
+            }
+
+            btnRegister.setOnClickListener {
+                navigateToRegister()
+            }
+        }
+    }
+
+    private fun installSplash() {
+        if (Build.VERSION_CODES.S <= Build.VERSION.SDK_INT) {
+            installSplashScreen().apply {
+                setKeepOnScreenCondition {
+                    !hasLoaded
+                }
+
+                setOnExitAnimationListener {
+                    if (newUser && hasLoaded) {
+                        it.remove()
+                    } else if (!newUser && hasLoaded) {
+                        navigateToMain()
+                    }
+                }
+            }
+        } else {
+            if (!newUser && hasLoaded) {
+                navigateToMain()
+            }
+        }
+    }
+
+    private fun hideStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+    }
+
+    private fun checkUser() {
+        onboardViewModel.apply {
+            isNew.observe(this@OnboardActivity) {
+                newUser = it
+            }
+
+            isLoading.observe(this@OnboardActivity) {
+                hasLoaded = !it
+            }
+        }
+    }
+
+    private fun setUser() {
+        onboardViewModel.setNewUser(false)
+    }
+
+    private fun navigateToMain() {
+        setUser()
+        Intent(this@OnboardActivity, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+
+    private fun navigateToLogin() {
+        Intent(this@OnboardActivity, LoginActivity::class.java).also {
+            startActivity(it)
+        }
+    }
+
+    private fun navigateToRegister() {
+        Intent(this@OnboardActivity, RegisterActivity::class.java).also {
+            startActivity(it)
+        }
     }
 }
--- app/src/main/java/com/agronify/android/viewmodel/OnboardViewModel.kt
+++ app/src/main/java/com/agronify/android/viewmodel/OnboardViewModel.kt
@@ -1,4 +1,39 @@
 package com.agronify.android.viewmodel
 
-class OnboardViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.agronify.android.model.repository.AuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardViewModel @Inject constructor(
+    private val authRepository: AuthRepository,
+): ViewModel() {
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _isNew = MutableLiveData<Boolean>()
+    val isNew: LiveData<Boolean> = _isNew
+
+    init {
+        checkUser()
+    }
+
+    private fun checkUser() {
+        _isLoading.value = true
+        viewModelScope.launch {
+            _isNew.value = authRepository.getNew()
+            _isLoading.value = false
+        }
+    }
+
+    fun setNewUser(state: Boolean) {
+        viewModelScope.launch {
+            authRepository.saveNew(state)
+        }
+    }
 }
