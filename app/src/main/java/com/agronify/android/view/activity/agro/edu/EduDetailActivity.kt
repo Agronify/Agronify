364122c4383c9e0a78798e8a25acb7e64c7d6b75
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-05-30 22:55:17 +0700

    `update` edu detail activity

--- app/src/main/java/com/agronify/android/view/activity/agro/edu/EduDetailActivity.kt
+++ app/src/main/java/com/agronify/android/view/activity/agro/edu/EduDetailActivity.kt
@@ -1,12 +1,70 @@
 package com.agronify.android.view.activity.agro.edu
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.agronify.android.R
+import android.view.View
+import android.view.WindowInsets
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.agronify.android.BuildConfig.BUCKET_URL
+import com.agronify.android.databinding.ActivityEduDetailBinding
+import com.agronify.android.util.Constants.EXTRA_EDU_CONTENT
+import com.agronify.android.util.Constants.EXTRA_EDU_IMAGE
+import com.agronify.android.util.Constants.EXTRA_EDU_TAGS
+import com.agronify.android.util.Constants.EXTRA_EDU_TITLE
+import com.agronify.android.view.adapter.EduTagAdapter
+import com.bumptech.glide.Glide
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class EduDetailActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityEduDetailBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edu_detail)
+        setContentView(binding.root)
+        setupView()
+    }
+
+    private fun setupView() {
+        hideStatusBar()
+        setupAppBar()
+        showEdu()
+    }
+
+    private fun hideStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        }
+    }
+
+    private fun setupAppBar() {
+        binding.topAppBar.apply {
+            setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        }
+    }
+
+    private fun showEdu() {
+        binding.apply {
+            intent.apply {
+                val image = getStringExtra(EXTRA_EDU_IMAGE)
+                Glide.with(this@EduDetailActivity)
+                    .load(BUCKET_URL + image)
+                    .into(ivEduImage)
+                tvEduTitle.text = getStringExtra(EXTRA_EDU_TITLE)
+                tvEduDesc.text = getStringExtra(EXTRA_EDU_CONTENT)
+
+                val tags = getStringArrayListExtra(EXTRA_EDU_TAGS)
+                if (!tags.isNullOrEmpty()) {
+                    rvEduTag.apply {
+                        adapter = EduTagAdapter(tags)
+                        layoutManager = LinearLayoutManager(this@EduDetailActivity, LinearLayoutManager.HORIZONTAL, false)
+                    }
+                }
+            }
+        }
     }
 }
