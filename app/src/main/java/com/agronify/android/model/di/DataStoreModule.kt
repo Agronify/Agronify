d45c453736c8261ff7bab2c955711e5e365862c2
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-05-30 13:14:29 +0700

    `add` data store module and preference

---
+++ app/src/main/java/com/agronify/android/model/di/DataStoreModule.kt
@@ -0,0 +1,31 @@
+package com.agronify.android.model.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.agronify.android.model.local.UserPreference
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user")
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.dataStore
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserPreference(dataStore: DataStore<Preferences>): UserPreference {
+        return UserPreference(dataStore)
+    }
+}
--- app/src/main/java/com/agronify/android/model/local/UserPreference.kt
+++ app/src/main/java/com/agronify/android/model/local/UserPreference.kt
@@ -1,4 +1,63 @@
 package com.agronify.android.model.local
 
-class UserPreference {
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.agronify.android.model.remote.response.Auth
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class UserPreference @Inject constructor(private val dataStore: DataStore<Preferences>) {
+    suspend fun getNew(): Boolean? {
+        return dataStore.data.first()[NEW_KEY]
+    }
+
+    suspend fun saveNew(state: Boolean) {
+        dataStore.edit {
+            it[NEW_KEY] = state
+        }
+    }
+
+    suspend fun getToken(): String? {
+        return dataStore.data.first()[TOKEN_KEY]
+    }
+
+    suspend fun saveToken(token: String) {
+        dataStore.edit {
+            it[TOKEN_KEY] = token
+        }
+    }
+
+    suspend fun getUser(): Auth {
+        return dataStore.data.first().let {
+            Auth(
+                it[NAME_KEY] ?: "",
+                it[EMAIL_KEY] ?: ""
+            )
+        }
+    }
+
+    suspend fun saveUser(name: String, email: String) {
+        dataStore.edit {
+            it[NAME_KEY] = name
+            it[EMAIL_KEY] = email
+        }
+    }
+
+    suspend fun clearUser() {
+        dataStore.edit {
+            it.remove(TOKEN_KEY)
+            it.remove(NAME_KEY)
+            it.remove(EMAIL_KEY)
+        }
+    }
+
+    companion object {
+        private val TOKEN_KEY = stringPreferencesKey("token")
+        private val NAME_KEY = stringPreferencesKey("name")
+        private val EMAIL_KEY = stringPreferencesKey("email")
+        private val NEW_KEY = booleanPreferencesKey("new")
+    }
 }
