b08857a1ff0c8bd0ae924b4c2c258c2d190ea0e7
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-05-30 13:13:14 +0700

    `update` auth network layer

--- app/src/main/java/com/agronify/android/model/remote/response/Auth.kt
+++ app/src/main/java/com/agronify/android/model/remote/response/Auth.kt
@@ -1,4 +1,25 @@
 package com.agronify.android.model.remote.response
 
-class Auth {
-}
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field:SerializedName("name")
+    val name: String,
+
+    @field:SerializedName("email")
+    val email: String,
+
+    @field:SerializedName("token")
+    val token: String
+)
+
+data class Auth(
+    @field:SerializedName("name")
+    val name: String,
+
+    @field:SerializedName("email")
+    val email: String
+)
--- app/src/main/java/com/agronify/android/model/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/agronify/android/model/remote/retrofit/ApiService.kt
@@ -1,11 +1,24 @@
 package com.agronify.android.model.remote.retrofit
 
+import com.agronify.android.model.remote.response.Auth
 import com.agronify.android.model.remote.response.CurrentWeatherResponse
 import com.agronify.android.model.remote.response.ForecastWeatherResponse
+import com.agronify.android.model.remote.response.LoginResponse
+import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.http.*
 
 interface ApiService {
+    @POST("auth/signin")
+    fun userLogin(
+        @Body request: JsonObject
+    ): Call<LoginResponse>
+
+    @POST("auth/signup")
+    fun userRegister(
+        @Body request: JsonObject
+    ): Call<Auth>
+
     @GET("weather")
     fun getCurrentWeather(
         @Query("type") type: String,
