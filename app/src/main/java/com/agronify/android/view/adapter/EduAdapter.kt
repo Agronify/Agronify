ce66ff5be2c317480fa04ff962f94466df7efb7d
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-05-30 22:53:19 +0700

    `update` edu adapter

--- app/src/main/java/com/agronify/android/view/adapter/EduAdapter.kt
+++ app/src/main/java/com/agronify/android/view/adapter/EduAdapter.kt
@@ -1,4 +1,55 @@
 package com.agronify.android.view.adapter
 
-class EduAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.agronify.android.BuildConfig.BUCKET_URL
+import com.agronify.android.databinding.ItemEduBinding
+import com.agronify.android.model.remote.response.Edu
+import com.agronify.android.util.EduDiffCallback
+import com.bumptech.glide.Glide
+
+class EduAdapter : ListAdapter<Edu, EduAdapter.ViewHolder>(EduDiffCallback()) {
+    private var onClickCallback: OnClickCallback? = null
+
+    interface OnClickCallback {
+        fun onClicked(edu: Edu)
+    }
+
+    fun setOnClickCallback(onClickCallback: OnClickCallback?) {
+        this.onClickCallback = onClickCallback
+    }
+
+    inner class ViewHolder(val binding: ItemEduBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemEduBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        getItem(position)?.let { edu ->
+            holder.apply {
+                binding.apply {
+                    Glide.with(itemView.context)
+                        .load(BUCKET_URL + edu.image)
+                        .into(ivEduImage)
+                    tvEduTitle.text = edu.title
+                    tvEduDesc.text = edu.content
+                    if (edu.tags.size > 2) {
+                        tvEduTag1.text = edu.tags[0]
+                        tvEduTag2.text = edu.tags[1]
+                        tvEduTagMore.text = "..."
+                    } else {
+                        tvEduTag1.text = edu.tags[0]
+                        tvEduTag2.text = edu.tags[1]
+                        tvEduTagMore.visibility = ViewGroup.GONE
+                    }
+                }
+
+                itemView.setOnClickListener { onClickCallback?.onClicked(edu) }
+            }
+        }
+    }
 }
--- app/src/main/java/com/agronify/android/view/adapter/EduTagAdapter.kt
+++ app/src/main/java/com/agronify/android/view/adapter/EduTagAdapter.kt
@@ -1,4 +1,28 @@
 package com.agronify.android.view.adapter
 
-class EduTagAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.agronify.android.databinding.ItemEduTagBinding
+import java.util.ArrayList
+
+class EduTagAdapter(private val tagList: ArrayList<String>) : RecyclerView.Adapter<EduTagAdapter.ViewHolder>() {
+    inner class ViewHolder(val itemEduTagBinding: ItemEduTagBinding) : RecyclerView.ViewHolder(itemEduTagBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemEduTagBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return tagList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.apply {
+            itemEduTagBinding.apply {
+                tvEduTag.text = tagList[position]
+            }
+        }
+    }
 }
