c413af7ea2cfcf670ae73a82f8e42015f09d296c
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-06-05 20:58:33 +0700

    `update` scan add and camera fragment

--- app/src/main/java/com/agronify/android/view/activity/agro/scan/ScanAddActivity.kt
+++ app/src/main/java/com/agronify/android/view/activity/agro/scan/ScanAddActivity.kt
@@ -1,12 +1,115 @@
 package com.agronify.android.view.activity.agro.scan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.Intent.ACTION_PICK
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.agronify.android.R
+import com.agronify.android.databinding.ActivityScanAddBinding
+import com.agronify.android.util.Constants.EXTRA_SCAN_ID
+import com.agronify.android.util.Constants.EXTRA_SCAN_NAME
+import com.agronify.android.util.Constants.EXTRA_SCAN_TYPE
+import com.agronify.android.util.Constants.EXTRA_TOKEN
+import com.agronify.android.viewmodel.ScanViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ScanAddActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityScanAddBinding.inflate(layoutInflater)
+    }
+    private val scanViewModel: ScanViewModel by viewModels()
+    private var launcherIntentCameraX: ActivityResultLauncher<Intent>? = null
+    private var launcherIntentGallery: ActivityResultLauncher<Intent>? = null
+    private var userToken: String = ""
+    private var cropId: Int = 0
+    private var cropName: String = ""
+    private var cropType: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_add)
+        setContentView(binding.root)
+        checkCameraPermission()
+        setupView()
+        setupAction()
+    }
+
+    private fun checkCameraPermission() {
+        scanViewModel.apply {
+            cameraPermissionGranted.observe(this@ScanAddActivity) { isGranted ->
+                if (!isGranted) {
+                    requestPermission(this@ScanAddActivity)
+                }
+            }
+        }
+    }
+
+    private fun setupView() {
+        userToken = intent.getStringExtra(EXTRA_TOKEN).toString()
+        cropId = intent.getIntExtra(EXTRA_SCAN_ID, 0)
+        cropName = intent.getStringExtra(EXTRA_SCAN_NAME).toString()
+        cropType = intent.getStringExtra(EXTRA_SCAN_TYPE).toString()
+
+        setupAppBar()
+        setupLauncher()
+        getImage()
+    }
+
+    private fun setupAppBar() {
+        binding.apply {
+            topAppBar.apply {
+                title = getString(R.string.add_detect_title, cropName)
+                setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+            }
+        }
+    }
+
+    private fun setupLauncher() {
+        launcherIntentCameraX = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            scanViewModel.handleCameraActivityResult(it)
+        }
+
+        launcherIntentGallery = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            scanViewModel.handleGalleryActivityResult(it, applicationContext)
+        }
+    }
+
+    private fun getImage() {
+        binding.apply {
+            scanViewModel.apply {
+                isLoading.observe(this@ScanAddActivity) {
+
+                }
+            }
+        }
+    }
+
+    private fun setupAction() {
+        binding.apply {
+            btnCamera.setOnClickListener {
+                Intent(this@ScanAddActivity, ScanCameraActivity::class.java).also {
+                    launcherIntentCameraX?.launch(it)
+                }
+            }
+
+            btnGallery.setOnClickListener {
+                Intent(ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).also {
+                    launcherIntentGallery?.launch(it)
+                }
+            }
+
+            btnDetect.apply {
+                text = getString(R.string.add_detect_title, cropName)
+                setOnClickListener {
+                    scanViewModel.apply {
+
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/agronify/android/view/activity/agro/scan/ScanCameraActivity.kt
+++ app/src/main/java/com/agronify/android/view/activity/agro/scan/ScanCameraActivity.kt
@@ -1,12 +1,108 @@
 package com.agronify.android.view.activity.agro.scan
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.agronify.android.R
+import android.view.OrientationEventListener
+import android.view.View
+import android.view.WindowInsets
+import androidx.activity.viewModels
+import androidx.camera.core.CameraControl
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.agronify.android.databinding.ActivityScanCameraBinding
+import com.agronify.android.viewmodel.ScanViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ScanCameraActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityScanCameraBinding.inflate(layoutInflater)
+    }
+    private val scanViewModel: ScanViewModel by viewModels()
+    private var imageCapture: ImageCapture? = null
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private var camera: CameraControl? = null
+    private var orientationEventListener: OrientationEventListener? = null
+    private var imageRotationDegreesValue: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_camera)
+        setContentView(binding.root)
+        setupView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        imageCapture = null
+        orientationEventListener?.disable()
+        ProcessCameraProvider.getInstance(this@ScanCameraActivity).get().unbindAll()
+    }
+
+    private fun setupView() {
+        hideStatusBar()
+        setupAppBar()
+        setupCamera()
+        setupOrientation()
+    }
+
+    private fun hideStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        }
+    }
+
+    private fun setupAppBar() {
+        binding.apply {
+            topAppBar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        }
+    }
+
+    private fun setupCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this@ScanCameraActivity)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().apply {
+                setSurfaceProvider(binding.viewfinder.surfaceProvider)
+            }
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this@ScanCameraActivity, cameraSelector, preview, imageCapture)
+                camera = cameraProvider.bindToLifecycle(this@ScanCameraActivity, cameraSelector, preview, imageCapture).cameraControl
+                binding.apply {
+                    camera!!.enableTorch(true)
+                    var useFlash = true
+                    btnFlash.setOnClickListener {
+                        useFlash = !useFlash
+                        camera!!.enableTorch(useFlash)
+                    }
+                }
+            } catch (_: Exception) {}
+        }, ContextCompat.getMainExecutor(this@ScanCameraActivity))
+    }
+
+    private fun setupOrientation() {
+        orientationEventListener = object : OrientationEventListener(this) {
+            override fun onOrientationChanged(orientation: Int) {
+                val rotation = when (orientation) {
+                    in 45..134 -> 180
+                    in 135..224 -> 270
+                    in 225..314 -> 0
+                    else -> 90
+                }
+                if (imageRotationDegreesValue != rotation) {
+                    imageRotationDegreesValue = rotation
+                    imageCapture?.targetRotation = rotation
+                }
+            }
+        }
+        orientationEventListener?.enable()
     }
 }
