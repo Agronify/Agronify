c10019901bf7df0f43c15529191f619749e24268
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-06-12 11:54:54 +0700

    `update` extra constants for camera

--- app/src/main/java/com/agronify/android/util/Constants.kt
+++ app/src/main/java/com/agronify/android/util/Constants.kt
@@ -9,13 +9,19 @@ object Constants {
     const val EXTRA_LOCATION = "extra_location"
     const val EXTRA_TOKEN = "extra_token"
     const val EXTRA_LOGIN = "extra_login"
-    const val EXTRA_EDU_ID = "extra_edu_id"
+
     const val EXTRA_EDU_TITLE = "extra_edu_title"
     const val EXTRA_EDU_IMAGE = "extra_edu_image"
     const val EXTRA_EDU_CONTENT = "extra_edu_content"
     const val EXTRA_EDU_TAGS = "extra_edu_tags"
+
     const val EXTRA_SCAN_ID = "extra_scan_id"
     const val EXTRA_SCAN_NAME = "extra_scan_name"
     const val EXTRA_SCAN_TYPE = "extra_scan_type"
     const val EXTRA_SCAN_IMAGE = "extra_scan_image"
+    const val EXTRA_SCAN_SUCCESS = "extra_scan_success"
+    const val EXTRA_SCAN_RESULT = "extra_scan_result"
+
+    const val CAMERA_X_SUCCESS = 201
+    const val CAMERA_X_ROTATION = "image_rotation_degree"
 }
