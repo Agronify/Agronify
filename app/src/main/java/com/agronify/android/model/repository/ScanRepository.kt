ec05e2fd4d7b6faa0f59e20a81f64f12c4488741
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-06-12 11:53:20 +0700

    `update` prediction repository

--- app/src/main/java/com/agronify/android/model/repository/ScanRepository.kt
+++ app/src/main/java/com/agronify/android/model/repository/ScanRepository.kt
@@ -1,9 +1,14 @@
 package com.agronify.android.model.repository
 
+import com.agronify.android.model.remote.response.PredictResponse
 import com.agronify.android.model.remote.response.Scan
+import com.agronify.android.model.remote.response.UploadResponse
 import com.agronify.android.model.remote.retrofit.ApiService
+import com.google.gson.JsonObject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.await
 import javax.inject.Inject
 import javax.inject.Named
@@ -17,6 +22,18 @@ class ScanRepository @Inject constructor(
     suspend fun getScan(): List<Scan> {
         return withContext(networkDispatcher) {
             apiService.getScan().await()
+        }
+    }
+
+    suspend fun uploadScan(token: String, file: MultipartBody.Part, type: RequestBody): UploadResponse {
+        return withContext(networkDispatcher) {
+            apiService.uploadScan("Bearer $token", file, type).await()
+        }
+    }
+
+    suspend fun predictScan(token: String, request: JsonObject): PredictResponse {
+        return withContext(networkDispatcher) {
+            apiService.predictScan("Bearer $token", request).await()
         }
     }
 }
