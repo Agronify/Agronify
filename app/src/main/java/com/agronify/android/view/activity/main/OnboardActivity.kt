06944bc2994a703a8539b0b7a9a61c78769bfbba
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-06-12 12:07:15 +0700

    `update` handle location permission

--- app/src/main/java/com/agronify/android/view/activity/main/OnboardActivity.kt
+++ app/src/main/java/com/agronify/android/view/activity/main/OnboardActivity.kt
@@ -1,17 +1,18 @@
 package com.agronify.android.view.activity.main
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.view.WindowManager
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.agronify.android.R
 import com.agronify.android.databinding.ActivityOnboardBinding
 import com.agronify.android.viewmodel.OnboardViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,6 +25,17 @@ class OnboardActivity : AppCompatActivity() {
     private val onboardViewModel: OnboardViewModel by viewModels()
     private var newUser: Boolean = false
     private var hasLoaded: Boolean = false
+    private var hasPermission: Boolean = false
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        hasPermission = when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> true
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> true
+            else -> false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,23 +67,27 @@ class OnboardActivity : AppCompatActivity() {
     }
 
     private fun installSplash() {
-        if (Build.VERSION_CODES.S <= Build.VERSION.SDK_INT) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             installSplashScreen().apply {
                 setKeepOnScreenCondition {
-                    !hasLoaded
+                    !hasLoaded && !hasPermission
                 }
 
                 setOnExitAnimationListener {
-                    if (newUser && hasLoaded) {
+                    if (newUser && hasLoaded && hasPermission) {
                         it.remove()
-                    } else if (!newUser && hasLoaded) {
+                    } else if (!newUser && hasLoaded && hasPermission) {
                         navigateToMain()
                     }
                 }
             }
         } else {
             if (!newUser && hasLoaded) {
-                navigateToMain()
+                if (hasPermission) {
+                    navigateToMain()
+                } else {
+                    requestLocationPermission()
+                }
             }
         }
     }
@@ -84,6 +100,27 @@ class OnboardActivity : AppCompatActivity() {
         }
     }
 
+    private fun requestLocationPermission() {
+        val granted = LOCATION_PERMISSION.all {
+            ContextCompat.checkSelfPermission(applicationContext, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (granted) {
+            hasPermission = true
+        } else {
+            val shouldShowRationale = LOCATION_PERMISSION.any {
+                ActivityCompat.shouldShowRequestPermissionRationale(this, it)
+            }
+
+            if (shouldShowRationale) {
+                hasPermission = false
+            } else {
+                hasPermission = false
+                locationPermissionRequest.launch(LOCATION_PERMISSION)
+            }
+        }
+    }
+
     private fun checkUser() {
         onboardViewModel.apply {
             isNew.observe(this@OnboardActivity) {
@@ -92,6 +129,7 @@ class OnboardActivity : AppCompatActivity() {
 
             isLoading.observe(this@OnboardActivity) {
                 hasLoaded = !it
+                requestLocationPermission()
             }
         }
     }
@@ -117,6 +155,19 @@ class OnboardActivity : AppCompatActivity() {
     private fun navigateToRegister() {
         Intent(this@OnboardActivity, RegisterActivity::class.java).also {
             startActivity(it)
+        }
+    }
+
+    private companion object {
+        val LOCATION_PERMISSION = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        } else {
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
         }
     }
 }
