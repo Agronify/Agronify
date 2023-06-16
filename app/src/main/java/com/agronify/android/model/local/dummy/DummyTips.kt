2e0665da3e332c046b42fd98f438682e0748b194
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-06-16 22:22:24 +0700

    `update` agro tips

--- app/src/main/java/com/agronify/android/model/local/dummy/DummyTips.kt
+++ app/src/main/java/com/agronify/android/model/local/dummy/DummyTips.kt
@@ -8,31 +8,31 @@ object DummyTips {
                 listOf(
                     Tip(
                         "Rekomendasi",
-                        "Gulma dapat dengan mudah dihilangkan di langit cerah, karena akan lebih terlihat dengan latar belakang langit."
+                        "Gulma dapat dengan mudah dihilangkan di langit cerah."
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Pangkas tanaman selama cuaca cerah untuk meningkatkan sirkulasi udara, paparan sinar matahari, dan kesehatan tanaman secara keseluruhan."
+                        "Pangkas tanaman selama cuaca cerah untuk meningkatkan sirkulasi udara."
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Terapkan pupuk atau kompos ke tanah selama cuaca cerah untuk memungkinkan nutrisi meresap secara efektif dan meminimalkan limpasan."
+                        "Terapkan pupuk atau kompos ke tanah selama cuaca cerah."
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Langit cerah bagus untuk memanen buah, sayuran, dan biji-bijian, karena cuaca kering membantu mencegah pembusukan dan memungkinkan pengeringan dan penyimpanan stok yang efisien."
+                        "Panen buah dan sayuran. Langit cerah bagus untuk memanen buah."
                     ),
                     Tip(
                         "Hindari",
-                        "Hindari penggunaan pestisida selama cuaca langit cerah karena dapat menguap dengan cepat, mengurangi keefektifannya."
+                        "Hindari penggunaan pestisida selama cuaca langit cerah."
                     ),
                     Tip(
                         "Hindari",
-                        "Hindari mentransplantasikan bibit yang rapuh atau muda selama cuaca cerah, karena tidak adanya hujan dapat membuat tanaman stres, membuatnya lebih rentan terhadap kejutan transplantasi."
+                        "Hindari mentransplantasikan bibit yang rapuh selama cuaca cerah."
                     ),
                     Tip(
                         "Hindari",
-                        "Tidak disarankan untuk mengaplikasikan atau mengisi kembali mulsa  selama cuaca langit cerah, karena dapat mencegah air hujan mencapai tanah dan dapat menyebabkan stres kekeringan pada tanaman."
+                        "Tidak disarankan untuk menambah mulsa selama cuaca langit cerah."
                     )
                 )
             ),
@@ -41,15 +41,11 @@ object DummyTips {
                 listOf(
                     Tip(
                         "Rekomendasi",
-                        "Siram tanaman Anda, menyiram tanaman pada cuaca ini memungkinkan penyerapan optimal dan mengurangi risiko penguapan."
+                        "Siram tanaman Anda karena penyerapan optimal saat cerah berawan."
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Tutupan awan dan suhu yang lebih rendah menciptakan lingkungan yang lebih baik untuk tanaman yang baru ditanam atau ditransplantasikan."
-                    ),
-                    Tip(
-                        "Rekomendasi",
-                        "Paparan sinar matahari yang berkurang dapat membantu mencegah stres berlebihan pada tanaman muda, memungkinkan mereka membangun sistem akar dengan lebih efektif."
+                        "Tanam atau pindah tanaman baru saat cuaca berawan."
                     )
                 )
             ),
@@ -57,20 +53,16 @@ object DummyTips {
                 "overcast",
                 listOf(
                     Tip(
-                        "Rekomendasi",
-                        "Kondisi cuaca mendung dapat menciptakan lingkungan yang kondusif bagi hama dan penyakit tertentu. Manfaatkan waktu ini untuk mencari potensi masalah, menerapkan tindakan pencegahan, dan menerapkan perawatan yang tepat jika diperlukan."
+                        "Hindari",
+                        "Hindari memanen buah dan sayur dalam kondisi mendung."
                     ),
                     Tip(
                         "Hindari",
-                        "Hindari memanen buah dan sayur. Dalam kondisi mendung, hasil panen dapat mempertahankan lebih banyak kelembaban, membuatnya lebih rentan terhadap penyakit busuk atau jamur."
+                        "Hindari mengaplikasikan pestisida atau herbisida."
                     ),
                     Tip(
                         "Hindari",
-                        "Hindari mengaplikasikan pestisida atau herbisida. Hujan atau kelembapan dari cuaca mendung dapat menghanyutkan pestisida atau herbisida sebelum sempat diserap atau berpengaruh."
-                    ),
-                    Tip(
-                        "Hindari",
-                        "Cuaca mendung dengan tingkat kelembapan tinggi tidak kondusif untuk mengeringkan tanaman di luar ruangan."
+                        "Cuaca mendung tidak kondusif untuk mengeringkan tanaman."
                     )
                 )
             ),
@@ -79,27 +71,23 @@ object DummyTips {
                 listOf(
                     Tip(
                         "Rekomendasi",
-                        "Penyemprotan foliar. Gerimis dapat membantu mendistribusikan semprotan foliar secara merata, seperti pupuk organik, insektisida, atau fungisida, pada tanaman. Kelembaban dari gerimis dapat membantu semprotan menempel pada dedaunan secara efektif."
+                        "Lakukan penyemprotan foliar saat gerimis."
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Panen sayuran hijau dan herbal. Sayuran hijau dan herba sering mendapat manfaat dari gerimis karena memberikan kelembaban alami tanpa penyiraman berlebihan. Ini adalah waktu yang tepat untuk memanen tanaman ini karena cenderung lebih segar dan beraroma."
+                        "Panen sayuran hijau dan herbal saat gerimis."
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Pemindahan bibit. Tanah lembap akibat gerimis dapat mempermudah pemindahan bibit karena membantu pembentukan akar. Berhati-hatilah agar tidak merusak akar halus saat melakukan transplantasi."
+                        "Lakukan pemindahan bibit saat gerimis."
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Gerimis membantu melembabkan tanah, sehingga cocok untuk kegiatan seperti persiapan bedengan, mengolah, atau menambahkan bahan organik. Hal ini dapat meningkatkan struktur tanah dan ketersediaan unsur hara."
+                        "Gerimis membantu melembabkan tanah Anda."
                     ),
                     Tip(
                         "Hindari",
-                        "Hindari pengolahan atau penanaman yang berlebihan selama gerimis, karena dapat menyebabkan erosi tanah dan hilangnya unsur hara. Jika budidaya diperlukan, berhati-hatilah dan minimalkan sejauh mana tanah terganggu."
-                    ),
-                    Tip(
-                        "Hindari",
-                        "Sebaiknya hindari menanam benih atau bibit yang sangat sensitif terhadap kelembaban berlebih selama gerimis. Tanaman seperti itu mungkin rentan membusuk atau mengembangkan penyakit jamur."
+                        "Hindari pengolahan atau penanaman yang berlebihan selama gerimis."
                     )
                 )
             ),
@@ -108,31 +96,23 @@ object DummyTips {
                 listOf(
                     Tip(
                         "Rekomendasi",
-                        "Lindungi tanaman yang sensitif. Anda perlu mengevaluasi perlunya tindakan perlindungan tambahan seperti menutup tanaman sensitif, jika hujan deras disertai angin kencang."
+                        "Pancuran hujan dapat membantu mendistribusikan pupuk."
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Pancuran hujan dapat membantu mendistribusikan pupuk yang diterapkan secara merata pada tanaman."
-                    ),
-                    Tip(
-                        "Rekomendasi",
-                        "Tanah lembab selama hujan sedang dapat membantu keberhasilan pembentukan transplantasi."
-                    ),
-                    Tip(
-                        "Rekomendasi",
-                        "Menerapkan mulsa di sekitar tanaman dapat membantu mengurangi erosi tanah yang disebabkan oleh hujan sedang dan menjaga kelembapan."
+                        "Menerapkan mulsa saat hujan dapat membantu mengurangi erosi tanah."
                     ),
                     Tip(
                         "Hindari",
-                        "Sebaiknya hindari penggunaan pestisida atau pupuk selama hujan untuk mencegah limpasan dan potensi pencemaran lingkungan."
+                        "Sebaiknya hindari penggunaan pestisida atau pupuk selama hujan."
                     ),
                     Tip(
                         "Hindari",
-                        "Irigasi biasanya tidak diperlukan saat hujan, jadi sebaiknya hindari menyiram tanaman selama periode ini."
+                        "Hindari menyiram tanaman selama periode ini."
                     ),
                     Tip(
                         "Hindari",
-                        "Hindari penyemprotan pestisida atau herbisida. Hujan dapat menghanyutkan bahan kimia, mengurangi keefektifannya dan berpotensi menyebabkan pencemaran lingkungan."
+                        "Hindari penyemprotan pestisida atau herbisida saat hujan."
                     )
                 )
             ),
@@ -145,7 +125,7 @@ object DummyTips {
                     ),
                     Tip(
                         "Rekomendasi",
-                        "Hindari kegiatan pertanian apa pun seperti menanam, memanen, atau kerja lapangan sampai badai berlalu untuk memastikan keselamatan Anda."
+                        "Hindari kegiatan pertanian apa pun sampai badai berlalu."
                     )
                 )
             ),
@@ -154,15 +134,15 @@ object DummyTips {
                 listOf(
                     Tip(
                         "Rekomendasi",
-                        "Cuaca berawan dapat memberikan kondisi yang menguntungkan bagi tanaman di rumah kaca. Periksa suhu dan tingkat kelembaban, pantau kesehatan tanaman, dan lakukan tugas yang diperlukan seperti menyiram dan memupuk."
+                        "Cuaca berawan baik bagi tanaman di rumah kaca."
                     ),
                     Tip(
                         "Hindari",
-                        "Hindari Irigasi berlebihan. Dalam cuaca berawan, sudah ada kadar air yang lebih tinggi di udara. Hindari irigasi yang tidak perlu karena dapat menyebabkan kelembaban yang berlebihan, yang dapat menyebabkan penyakit dan pembusukan pada tanaman."
+                        "Hindari irigasi berlebihan saat cuaca berawan."
                     ),
                     Tip(
                         "Hindari",
-                        "Tunda aktivitas pemangkasan selama cuaca berawan. Kelembaban dapat meningkatkan penyebaran penyakit, dan berkurangnya visibilitas dapat menyebabkan pemangkasan yang tidak tepat, menyebabkan kerusakan pada tanaman."
+                        "Tunda aktivitas pemangkasan selama cuaca berawan."
                     )
                 )
             )
